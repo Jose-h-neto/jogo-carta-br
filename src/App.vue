1a41f0adcc7809c8be361d26ba5abfc4bf12f6ea
Jose-Neto <jose.h.neto@outlook.com>
2026-09-04 01:04:47 -0300

    criacao do protótipo do card

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Card from '@/components/Cards/Card.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Card
+    :card-data="{
+      badge: 'Início',
+      name: 'Card de exemplo',
+      info: 'Info do card de exemplo',
+      year: '2024',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Getulio_Vargas_%281930%29.jpg/500px-Getulio_Vargas_%281930%29.jpg?utm_source=www.wikidata.org&utm_campaign=rest&utm_content=thumbnail',
+    }"
+  />
 </template>
 
 <style scoped></style>
